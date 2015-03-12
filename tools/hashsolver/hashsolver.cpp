1a6decb828fc6a6f7e350f55a42208197f8e0a8f
Sebastian Knopp <sbstn.knopp@googlemail.com>
2015-03-12 01:05:33 +0100

    removed experiment for painter challenge

--- tools/hashsolver/hashsolver.cpp
+++ tools/hashsolver/hashsolver.cpp
@@ -1,40 +1,14 @@
-#include "Canvas.h"
-#include "Command.h"
-#include "GreedySolver.h"
 #include <iostream>
 #include <fstream>
 #include <limits>
 
 
-Commands solveTrivially(const base::Vector2d<State>& a_vector)
-{
-    Commands result;
-    for (size_t x = 0; x != a_vector.width(); ++x)
-    {
-        for (size_t y = 0; y !=a_vector.height(); ++y)
-        {
-            if (a_vector(x, y) == State::Black)
-                result.emplace_back(Command::Type::PaintSQ, x, y, 0);
-        }
-    }
-    return result;
-}
-
 
 int main (int , char *[])
 {
-    std::cout << "Hello World!" << std::endl;
-
-    base::Vector2d<State> v = readFile("doodle.txt");
-
-    Commands result = solveGreedily(v);
-    //Commands result = solveTrivially(v);
-
-    for (const auto& r : result)
-        std::cout << r << std::endl;
-    std::cout << "nmbOfCommands " << result.size() << "\n";
-
-    verify(v, result);
+    std::cout << "Google Hash 2015" << std::endl;
+    std::cout << "Team A211: Abdel Bitar and Sebastian Knopp" << std::endl;
 }
+
 
 
--- tools/hashsolver/test/GreedySolverTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "GreedySolver.h"
-#include "gtest/gtest.h"
-
-
-TEST(GreedySolverTest, empty)
-{
-    EXPECT_TRUE(true);
-}
-
