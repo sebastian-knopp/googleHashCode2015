e1f0dbc7af549bf13e5eb5cfe9edebe6f11dc6ea
Sebastian Knopp <sbstn.knopp@googlemail.com>
2015-03-12 18:03:55 +0100

    test a line

--- tools/hashsolver/hashsolver.cpp
+++ tools/hashsolver/hashsolver.cpp
@@ -7,6 +7,7 @@
 int main (int , char *[])
 {
     std::cout << "Google Hash 2015" << std::endl;
+    std::cout << "----------------" << std::endl;
     std::cout << "Team A211: Abdel Bitar and Sebastian Knopp" << std::endl;
 }
 
