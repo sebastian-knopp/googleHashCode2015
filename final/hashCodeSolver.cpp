aabe4f0f59c48bf3d3458c64121cb3346537d7f8
Abdoul Bitar <bitar@sgc-pcp-bitar.(none)>
2015-03-23 17:51:45 +0100

    compute exponential

--- final/hashCodeSolver.cpp
+++ final/hashCodeSolver.cpp
@@ -1,9 +1,24 @@
 
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
     std::cout << "hello world" << std::endl;
+
+    int i;
+    double e=1.0;
+    double facto=1;
+
+    for(i=1;i<=10000;i++){
+        facto=facto*i;
+        e+=1/facto;
+    }
+
+    std::cout<<"Une approximation du nombre de Neper : "<<std::setprecision(10)<<e<<std::endl<<std::endl;
+
+    return 0;
+
 }
 
 
