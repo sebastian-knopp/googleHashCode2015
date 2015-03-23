8b71c9cb676befcec7870b47a83d0036e3b59c74
Abdoul Bitar <bitar@sgc-pcp-bitar.(none)>
2015-03-23 18:11:05 +0100

    end of git training

--- final/hashCodeSolver.cpp
+++ final/hashCodeSolver.cpp
@@ -8,17 +8,6 @@ int main()
     std::cout << std::endl;
     std::cout << "hello world" << std::endl;
 
-    int i;
-    double e=1.0;
-    double facto=1;
-
-    for(i=1;i<=5000;i++){
-        facto=facto*i;
-        e+=1/facto;
-    }
-
-    std::cout<<"Une approximation du nombre de Neper : "<<std::setprecision(10)<<e<<std::endl<<std::endl;
-
     return 0;
 
 }
