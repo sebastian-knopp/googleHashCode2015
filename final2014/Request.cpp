033d84af263c477ab315dbac4deff39ba76f3503
Sebastian Knopp <abdlbitar@gmail.com>
2015-03-24 16:47:28 +0100

    read junctions

--- final2014/Request.cpp
+++ final2014/Request.cpp
@@ -1,21 +1,38 @@
 #include "Request.h"
 
 
+std::istream& operator>>(std::istream& a_is, Coordinate& a_coord)
+{
+    a_is >> a_coord.m_lat;
+    a_is >> a_coord.m_long;
+    return a_is;
+}
+
 
 std::istream& operator>>(std::istream& a_is, Request& a_request)
 {
-    a_is >> a_request.m_nmbJunctions;
+    size_t nmbJunctions = 0;
+    a_is >> nmbJunctions;
     a_is >> a_request.m_nmbStreets;
     a_is >> a_request.m_availableSecondsPerCar;
     a_is >> a_request.m_nmbCars;
     a_is >> a_request.m_initialJunctionIndex;
+
+    a_request.m_junctions.reserve(nmbJunctions);
+    for (size_t i = 0; i != nmbJunctions; ++i)
+    {
+        Coordinate junction;
+        a_is >> junction;
+        a_request.m_junctions.push_back(junction);
+    }
+
     return a_is;
 }
 
 
 std::ostream& operator<<(std::ostream& a_os, const Request& a_request)
 {
-    a_os << " " << a_request.m_nmbJunctions;
+    a_os << " " << a_request.m_junctions.size();
     a_os << " " << a_request.m_nmbStreets;
     a_os << " " << a_request.m_availableSecondsPerCar;
     a_os << " " << a_request.m_nmbCars;
@@ -23,4 +40,5 @@ std::ostream& operator<<(std::ostream& a_os, const Request& a_request)
     a_os << std::endl;
     return a_os;
 }
+
 
--- final2014/Request.h
+++ final2014/Request.h
@@ -1,5 +1,18 @@
 #pragma once
 #include <istream>
+#include <vector>
+
+
+struct Coordinate
+{
+    double m_lat;
+    double m_long;
+};
+
+typedef std::vector<Coordinate> Coordinates;
+
+std::istream& operator>>(std::istream& a_is, Coordinate& a_coord);
+
 
 class Request {
 
@@ -12,14 +25,14 @@ public:
 
 public:
 
-    size_t m_nmbJunctions = 0; // denotes the number of junctions in the city
     size_t m_nmbStreets = 0; // denotes the number of streets in the city
     size_t m_availableSecondsPerCar = 0; // denotes the virtual time in seconds allowed for the car itineraries
     size_t m_nmbCars = 0; // denotes the number of cars in the fleet
     size_t m_initialJunctionIndex = 0; // denotes the junction at which all the cars are located initially
 
-};
+    Coordinates m_junctions;
 
+};
 
 std::istream& operator>>(std::istream& a_is, Request& a_request);
 
