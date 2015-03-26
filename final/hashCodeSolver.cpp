d14140ff7deef2b34a832180fdeff5dde92cc57f
Sebastian Knopp <sbstn.knopp@gmail.com>
2015-03-26 13:33:54 +0100

    basic file reading and exception handling

--- final/hashCodeSolver.cpp
+++ final/hashCodeSolver.cpp
@@ -1,10 +1,23 @@
+#include "Request.h"
 #include <iostream>
-#include <iomanip>
+#include <fstream>
 
 
+int solve(int, char* [])
+{
+    Request request;
+    std::ifstream ifs("example.in");
+    if (!ifs)
+        throw "Cannot open file.";
+
+    ifs >> request;
+    std::cout << request << std::endl;
+
+    return 0;
+}
 
 
-int main()
+int main(int a_argn, char* a_argv[])
 {
     std::cout << " --------------------------------------------------- " << std::endl;
     std::cout << "|         Google Hash Code Challenge 2015           |" << std::endl;
@@ -14,7 +27,28 @@ int main()
     std::cout << std::endl;
     std::cout << std::endl;
 
-    return 0;
+    try
+    {
+        return solve(a_argn, a_argv);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Caught std::exception: " << e.what() << std::endl;
+    }
+    catch(const std::string& s)
+    {
+        std::cerr << "Caught std::string: " << s << std::endl;
+    }
+    catch(const char* c)
+    {
+        std::cerr << "Caught char*: " << c << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "Caught unknown exception.";
+    }
+
+    return 1;
 }
 
 
