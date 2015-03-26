696004be16412430c09c5855f7184da3b11482e6
Sebastian Knopp <sbstn.knopp@googlemail.com>
2015-03-26 01:19:08 +0100

    removed unnecessary sorting

--- final2014/hashCodeSolver.cpp
+++ final2014/hashCodeSolver.cpp
@@ -5,16 +5,6 @@
 #include <algorithm>
 
 
-
-//Abdel
-//Comparing two streets, just to sort it correctly and give it as a parameter to the sort function of standard algorithm library
-bool compareStreets(Street s1, Street s2){
-    return ( ( (double)(s1.m_length)/(double)(s1.m_cost) > (double)(s2.m_length)/(double)(s2.m_cost)  ) || ( ( (double)(s1.m_length)/(double)(s1.m_cost) == (double)(s2.m_length)/(double)(s2.m_cost)) && (s1.m_length > s2.m_length)) );
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-
 int main()
 {
     std::cout << "Google Hash Code Challenge 2014- Team A211" << std::endl;
@@ -30,15 +20,6 @@ int main()
     }
 
     fs >> r;
-
-    //Abdel
-    //Sorting street list in the increasing order of length/cost
-    sort(r.m_streets.begin(),r.m_streets.end(),compareStreets);
-/*
-    //for(size_t i=0;i<r.m_streets.size();++i)
-    //    std::cout <<"length "<<r.m_streets[i].m_length<<" cost "<<r.m_streets[i].m_cost<<" ratio "<<(double)(r.m_streets[i].m_length)/(double)(r.m_streets[i].m_cost)<<std::endl;
-*/
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     r.computeAdjacentStreetIndices();
 
